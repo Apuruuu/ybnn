cb163bfcdef315e56504d904455d1facddca7f79
wu.desktop <wyf559@outlook.com>
2021-02-04 02:30:50 +0900

    Update dht11test.py

--- dht11test.py
+++ dht11test.py
@@ -1,11 +1,12 @@
 import dht11
 import Adafruit_ADS1x15
 
-def get_temp(pipe_sensor, pin=19):
-    instance = dht11.DHT11(pin)
-    while True:
-        result = instance.read()
 
-        print('temperature',result.temperature, 'humidity',result.humidity)
+instance = dht11.DHT11(pin)
+while True:
+    result = instance.read()
 
-        time.sleep(1)
+    print('temperature',result.temperature, 'humidity',result.humidity)
+
+    time.sleep(1)
+
