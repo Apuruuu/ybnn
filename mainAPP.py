1c641b42dda82f782699d0c17fdaf895afa6bb49
wu.desktop <wyf559@outlook.com>
2021-03-24 00:07:04 +0900

    fix

--- mainAPP.py
+++ mainAPP.py
@@ -29,27 +29,44 @@ def get_temp(pipe_sensor, pin=19):
         sleep(5)
 
 def get_ADC_value(pipe_sensor):
-    import adafruit_ads1x15.ads1015 as ADS
-    from adafruit_ads1x15.analog_in import AnalogIn
+    import Adafruit_ADS1x15
 
-    # Create the I2C bus
-    i2c = busio.I2C(board.SCL, board.SDA)
+    adc = Adafruit_ADS1x15.ADS1115()
+    # Choose a gain of 1 for reading voltages from 0 to 4.09V.
+    # Or pick a different gain to change the range of voltages that are read:
+    #  - 2/3 = +/-6.144V
+    #  -   1 = +/-4.096V
+    #  -   2 = +/-2.048V
+    #  -   4 = +/-1.024V
+    #  -   8 = +/-0.512V
+    #  -  16 = +/-0.256V
+    # See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
+    GAIN = 1
 
-    # Create the ADC object using the I2C bus
-    ads = ADS.ADS1015(i2c)
-
-    # Create single-ended input on channel 0
-    chan = AnalogIn(ads, ADS.P0)
-
-    # Create differential input between channel 0 and 1
-    # chan = AnalogIn(ads, ADS.P0, ADS.P1)
-
-    print("{:>5}\t{:>5}".format("raw", "v"))
-
+    # Print nice channel column headers.
+    print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*range(4)))
+    print('-' * 37)
+    # Main loop.
     while True:
-        print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
+        # Read all the ADC channel values in a list.
+        values = [0]*4
+        for i in range(4):
+            # Read the specified ADC channel using the previously set gain value.
+            values[i] = adc.read_adc(i, gain=GAIN)
+            # Note you can also pass in an optional data_rate parameter that controls
+            # the ADC conversion time (in samples/second). Each chip has a different
+            # set of allowed data rate values, see datasheet Table 9 config register
+            # DR bit values.
+            #values[i] = adc.read_adc(i, gain=GAIN, data_rate=128)
+            # Each value will be a 12 or 16 bit signed integer value depending on the
+            # ADC (ADS1015 = 12-bit, ADS1115 = 16-bit).
+        # Print the ADC values.
+        print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*values))
+        # Pause for half a second.
+        sleep(2)
+
         # pipe_sensor.send({'turbidity':values[0], 'PH':values[1], 'ADC3_A2':values[2], 'ADC4_A3':values[3]})
-        sleep(1)
+
         
 def get_time(pipe_sensor):
     while True:
