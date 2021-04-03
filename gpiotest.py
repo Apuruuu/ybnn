16a3ff708d12417a87e773bc471b6ec782e01470
wu.desktop <wyf559@outlook.com>
2021-04-04 04:36:06 +0900

    Update gpiotest.py

--- gpiotest.py
+++ gpiotest.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BCM)
 
@@ -6,4 +7,7 @@ GPIO_PIN_list = [14, 15, 18, 23, 24, 25, 8, 7, 4, 22]
 for PIN in GPIO_PIN_list:
     GPIO.setup(PIN, GPIO.OUT)
     GPIO.output(PIN, GPIO.HIGH)
+
+time.sleep(1)
+GPIO.cleanup()
 
