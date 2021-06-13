94dec288e3be1ad1a87641260bf212abcb1ce116
wu.desktop <wyf559@outlook.com>
2021-06-13 16:07:47 +0900

    Update mainAPP.py

--- mainAPP.py
+++ mainAPP.py
@@ -51,16 +51,14 @@ def get_ADC_value():
         # read ALL of ADC's channel
         values = [0]*4
         for i in range(4):
-            values[i] = units.ADS1115.readAdc(i)
+            values[i] = units.ADS1115.readAdc(i) * 6.144 / 32768 # 16bit(2^16) = 65536, half is minus
 
         # 计算PH
-        PH_volt = values[0] * 6.144 / 32768 # 16bit(2^16) = 65536, half is minus
-        PH_value = PH_volt * 3.5
+        PH_value = values[0] * 3.5
 
         # 计算浊度
-        turbidity_volt = values[1] * 6.144 / 32768
-        if turbidity_volt <= 5 and turbidity_volt >= 1 :
-            turbidity_percentage = 4.41 - (turbidity_volt * 0.8457)
+        if values[1] <= 5 and values[1] >= 1 :
+            turbidity_percentage = 4.41 - (values[1] * 0.8457)
             turbidity_NTU = turbidity_percentage * 1300 # 10-6(PPM) = 1ppm = 1mg/L = 0.13NTU(empirical formula), that is: 3.5% = 35000PPM = 35000mg/L = 4550NTU
         else: turbidity_NTU = -1
 
