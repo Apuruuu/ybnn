066a84783354f3fa518bcc662f8612ae5eeec3b6
wu.desktop <wyf559@outlook.com>
2021-06-21 15:40:27 +0900

    Update mainAPP.py

--- mainAPP.py
+++ mainAPP.py
@@ -54,7 +54,12 @@ def get_ADC_value():
             values[i] = units.ADS1115.readAdc(i) * 6.144 / 32768 # 16bit(2^16) = 65536, half is minus
 
         # 计算PH
-        PH_value = values[0] * 3.5
+        _temperature      = 25.0
+        _acidVoltage      = 2032.44
+        _neutralVoltage   = 1500.0
+        slope     = (7.0-4.0)/((_neutralVoltage-1500.0)/3.0 - (_acidVoltage-1500.0)/3.0)
+        intercept = 7.0 - slope*(_neutralVoltage-1500.0)/3.0
+        PH_value  = slope*(values[0]*1000-1500.0)/3.0+intercept
 
         # 计算浊度
         if values[1] <= 5 and values[1] >= 1 :
