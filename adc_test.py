b55b535ab833ebd7798712eef06437644d84e8ec
wu.desktop <wyf559@outlook.com>
2021-06-21 15:33:00 +0900

    Update adc_test.py

--- adc_test.py
+++ adc_test.py
@@ -2,6 +2,10 @@
 import units.ADS1115
 import time
 
+_temperature      = 25.0
+_acidVoltage      = 2032.44
+_neutralVoltage   = 1500.0
+
 while True:
     # read ALL of ADC's channel
     values = [0]*4
@@ -9,7 +13,11 @@ while True:
         values[i] = units.ADS1115.readAdc(i) * 6.144 / 32768 # 16bit(2^16) = 65536, half is minus
 
     # 计算PH
-    PH_value = values[0] * 3.5
+    slope     = (7.0-4.0)/((_neutralVoltage-1500.0)/3.0 - (_acidVoltage-1500.0)/3.0)
+    intercept = 7.0 - slope*(_neutralVoltage-1500.0)/3.0
+    PH_value  = slope*(values[0]*1000-1500.0)/3.0+intercept
+
+    # PH_value = values[0] * 3.5
 
     # 计算浊度
     if values[1] <= 5 and values[1] >= 1 :
